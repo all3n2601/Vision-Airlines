d2348cbbb76ebd3e9e969046ad95dda4f01b5112
HarishValliappan <harishvaliappan@gmail.com>
2024-03-06 21:59:28 +0530

    FlightSearch page updation

--- frontend/src/components/Shared/city_card.jsx
+++ frontend/src/components/Shared/city_card.jsx
@@ -3,8 +3,10 @@ import { FaMapMarkerAlt, FaMoneyBillWave } from 'react-icons/fa';
 
 const Card = ({ cityName, country, price, imageSrc }) => {
   return (
-    <div className="relative overflow-hidden rounded-lg shadow-lg hover:shadow-2xl border-2 border-gray-300 transition duration-200 ease-in-out hover:scale-105 group bg-white" style={{ height: "330px" ,fontFamily:"Anta"}}>
-      <img src={imageSrc} alt={cityName} className="object-cover rounded-lg w-full h-full transition-transform duration-300 ease-in-out group-hover:scale-110" />
+    <div className="relative overflow-hidden rounded-lg transition duration-300 ease-in-out group bg-white border-2 border-gray-300 hover:-translate-y-1 hover:scale-105"
+         style={{ height: "330px", fontFamily: "Anta", boxShadow: "0 10px 15px -3px rgba(0, 0, 0, 0.6), 0 4px 6px -4px rgba(0, 0, 0, 0.1)" }}>
+      <img src={imageSrc} alt={cityName} 
+           className="object-cover rounded-lg w-full h-full transition-transform duration-300 ease-in-out group-hover:scale-110" />
       <div className="absolute bottom-0 left-0 w-full p-4 bg-gradient-to-t from-black to-transparent text-white">
         <h3 className="text-xl font-bold flex items-center"><FaMapMarkerAlt className="mr-2" />{cityName}</h3>
         <p className="flex items-center"><FaMapMarkerAlt className="mr-2" />{country}</p>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'float': 'float 1s ease-out forwards', 
+      },
+      keyframes: {
+        float: {
+          '0%': { transform: 'translateY(20px)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+      },
+    },
   },
   plugins: [],
 }
